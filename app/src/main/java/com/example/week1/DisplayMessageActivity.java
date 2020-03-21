1a2600cd7f63e7b9b0cad6f0a6fd67c40508680a
Moria26 <Moria26@users.noreply.github.com>
2020-03-21 18:36:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/week1/DisplayMessageActivity.java
@@ -0,0 +1,45 @@
+package com.example.week1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+    private static final String TAG = "DisplayMessageActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+        Log.d(TAG, "DisplayMessageActivity");
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_pirates:
+                if (checked)
+                    Log.d(TAG, "使用正常");
+                    break;
+            case R.id.radio_ninjas:
+                if (checked)
+                    Log.d(TAG, "使用故障");
+                    break;
+        }
+    }
+}
